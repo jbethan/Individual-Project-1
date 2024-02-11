31ef535c853be40664ee97656eac699c4d3ae55b
Bethany Blake <jbethan@byu.edu>
2024-02-10 23:21:36 -0700

    fix nan to fill with 0 instead of drop

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from sklearn.neighbors import KNeighborsClassifier
 url1 = 'https://raw.githubusercontent.com/jbethan/Individual-Project-1/main/train.csv'
 df_train = pd.read_csv(url1, index_col=None)
 df_train = df_train.drop(columns=['name', 'neighborhood_overview', 'host_id', 'host_since', 'host_location', 'host_about', 'host_response_time', 'host_response_rate', 'host_acceptance_rate', 'host_is_superhost', 'host_listings_count', 'host_verifications', 'host_has_profile_pic', 'host_identity_verified', 'neighbourhood_cleansed', 'latitude', 'longitude', 'property_type', 'room_type', 'bathrooms_text', 'bedrooms', 'amenities',	'has_availability',	'first_review', 'last_review', 'instant_bookable'])
-df_train = df_train.dropna()
+df_train = df_train.fillna(0)
 
 Xtrain = df_train.drop(columns=['price', 'Id'])
 Ytrain = df_train['price'] 
@@ -19,7 +19,7 @@ Ytrain = df_train['price']
 url2 = 'https://raw.githubusercontent.com/jbethan/Individual-Project-1/main/test.csv'
 df_test = pd.read_csv(url2, index_col=None)
 df_test = df_test.drop(columns=['name', 'neighborhood_overview', 'host_id', 'host_since', 'host_location', 'host_about', 'host_response_time', 'host_response_rate', 'host_acceptance_rate', 'host_is_superhost', 'host_listings_count', 'host_verifications', 'host_has_profile_pic', 'host_identity_verified', 'neighbourhood_cleansed', 'latitude', 'longitude', 'property_type', 'room_type', 'bathrooms_text', 'bedrooms', 'amenities',	'has_availability',	'first_review', 'last_review', 'instant_bookable'])
-df_test = df_test.dropna()
+df_test = df_test.fillna(0)
 
 Xtest = df_test.drop(columns=['Id'])
 
