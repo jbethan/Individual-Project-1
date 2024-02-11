91fb41b67de2674bebb66148c90a40cf914cd29e
Bethany Blake <jbethan@byu.edu>
2024-02-10 23:16:31 -0700

    knearest neighbor modeling

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
 # %%
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.impute import SimpleImputer
-
-
-from sklearn.datasets import fetch_openml
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.linear_model import LogisticRegression
-from sklearn import metrics as skm
-import statsmodels.api as sm
-import statsmodels.formula.api as smf
+from sklearn.neighbors import KNeighborsClassifier
 
 # %%
-url = 'https://github.com/esnt/Data/raw/main/ISLR/Default.csv'
-df = pd.read_csv(url)
+# Train
+url1 = 'https://raw.githubusercontent.com/jbethan/Individual-Project-1/main/train.csv'
+df_train = pd.read_csv(url1, index_col=None)
+df_train = df_train.drop(columns=['name', 'neighborhood_overview', 'host_id', 'host_since', 'host_location', 'host_about', 'host_response_time', 'host_response_rate', 'host_acceptance_rate', 'host_is_superhost', 'host_listings_count', 'host_verifications', 'host_has_profile_pic', 'host_identity_verified', 'neighbourhood_cleansed', 'latitude', 'longitude', 'property_type', 'room_type', 'bathrooms_text', 'bedrooms', 'amenities',	'has_availability',	'first_review', 'last_review', 'instant_bookable'])
+df_train = df_train.dropna()
+
+Xtrain = df_train.drop(columns=['price', 'Id'])
+Ytrain = df_train['price'] 
 
 # %%
-pipe = Pipeline([
-    ("impute", SimpleImputer(strategy="mean")),
-    ("poly", PolynomialFeatures(degree=2, include_bias=False)),
-    ("scale", StandardScaler())
-])
+# Test
+url2 = 'https://raw.githubusercontent.com/jbethan/Individual-Project-1/main/test.csv'
+df_test = pd.read_csv(url2, index_col=None)
+df_test = df_test.drop(columns=['name', 'neighborhood_overview', 'host_id', 'host_since', 'host_location', 'host_about', 'host_response_time', 'host_response_rate', 'host_acceptance_rate', 'host_is_superhost', 'host_listings_count', 'host_verifications', 'host_has_profile_pic', 'host_identity_verified', 'neighbourhood_cleansed', 'latitude', 'longitude', 'property_type', 'room_type', 'bathrooms_text', 'bedrooms', 'amenities',	'has_availability',	'first_review', 'last_review', 'instant_bookable'])
+df_test = df_test.dropna()
 
-Xtrain = pipe.fit_transform(Xtrain)
-Xtest = pipe.transform(Xtest)
+Xtest = df_test.drop(columns=['Id'])
 
-#pipe.fit(x_train, y_train)
-#yhat = pipe.predict(x_test)
+# %%
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(Xtrain, Ytrain)
+
+# %%
+pred = knn.predict(Xtest)
+
+# %%
+df = pd.DataFrame({'Id': df_test['Id'], 'price': pred})
+
+# Write the DataFrame to a CSV file
+df.to_csv('submission.csv', index=False)
+
 # %%
